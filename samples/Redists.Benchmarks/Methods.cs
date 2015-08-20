d4bc3be7c21795ce236955f7d8210feeeefb0e90
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2015-08-20 09:35:40 +0200

    Refactor Benchmarks

---
+++ samples/Redists.Benchmarks/Methods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redists.Benchmarks
+{
+    public sealed class Methods
+    {
+        private static readonly Random Generator = new Random();
+
+        public static async Task AddAsync(ITimeSeriesClient client)
+        {
+            var at = DateTime.UtcNow.Date.AddSeconds(Generator.Next(3600 * 24));
+
+            await client.AddAsync(at, Generator.Next());
+        }
+
+        public static async Task Add100Async(ITimeSeriesClient client)
+        {
+            var start = DateTime.UtcNow.Date.AddSeconds(Generator.Next(3600 * 24));
+
+            //Full day
+            var tasks = new List<Task>();
+            for (var s = 0; s < 100; s++)
+            {
+                var now = start.AddSeconds(s);
+                tasks.Add(client.AddAsync(DateTime.UtcNow, Generator.Next()));
+            }
+
+            await Task.WhenAll(tasks.ToArray());
+        }
+
+        public static async Task AddBatchof100Async(ITimeSeriesClient client)
+        {
+            var start = DateTime.UtcNow.Date.AddSeconds(Generator.Next(3600 * 24));
+
+            //Full day
+            var points = new List<DataPoint>();
+            for (var s = 0; s < 100; s++)
+            {
+                var now = start.AddSeconds(s);
+                points.Add(new DataPoint(now, Generator.Next()));
+            }
+
+            await client.AddAsync(points.ToArray());
+        }
+
+        public static async Task ReadAllAsync(ITimeSeriesClient client)
+        {
+            var start = DateTime.UtcNow.Date;
+            var end = DateTime.UtcNow.Date.AddHours(1);
+
+            await client.AllAsync(start, end);
+        }
+    }
+}
--- samples/Redists.Benchmarks/Program.cs
+++ samples/Redists.Benchmarks/Program.cs
@@ -11,42 +11,75 @@ namespace Redists.Benchmarks
 {
     class Program
     {
-        static void Main(string[] args)
+        const int Iterations= 100;
+        static void Main()
         {
             Console.WriteLine("Redists.Benchmarks");
 
+            //setup connexion multiplexer
             var options = ConfigurationOptions.Parse("localhost:6379");
-            options.AbortOnConnectFail=false;
-            options.AllowAdmin=true;
+            options.AbortOnConnectFail = false;
+            options.AllowAdmin = true;
             var mux = ConnectionMultiplexer.Connect(options);
-            if(!mux.IsConnected)
+            if (!mux.IsConnected)
             {
                 Console.WriteLine("Could not connnect to redis instance.");
                 return;
             }
 
-            var tsOptions=new TimeSeriesOptions(3600*1000, 1, true, TimeSpan.FromHours(2));
-            var client = TimeSeriesFactory.New(mux.GetDatabase(0), "myts", tsOptions);
-            var gen = new Random();
-
-            var watcher = Stopwatch.StartNew();
-
-            var tasks = new List<Task>();
-            foreach(var i in Enumerable.Range(1, 5000))
+            //setup redists   
+            var tsOptions = new TimeSeriesOptions(3600 * 1000, 1, true, TimeSpan.FromDays(1));
+            
+            Profile("OneByOne", Iterations,() =>
             {
-                tasks.Add(client.AddAsync(DateTime.UtcNow, gen.Next() ));
-            }
+                var client = TimeSeriesFactory.New(mux.GetDatabase(0), "myts1", tsOptions);
+                Methods.AddAsync(client).Wait();
+            });
+            Profile("Add100", Iterations,() =>
+            {
+                var client = TimeSeriesFactory.New(mux.GetDatabase(0), "myts2", tsOptions);
+                Methods.Add100Async(client).Wait();
+            });
+            Profile("Batch", Iterations, () =>
+            {
+                var client = TimeSeriesFactory.New(mux.GetDatabase(0), "myts2", tsOptions);
+                Methods.AddBatchof100Async(client).Wait();
+            });
+            Profile("ReadAll", Iterations,() =>
+            {
+                var client = TimeSeriesFactory.New(mux.GetDatabase(0), "myts3", tsOptions);
+                Methods.ReadAllAsync(client).Wait();
+            });
 
-            Task.WaitAll(tasks.ToArray());
-            watcher.Stop();
-            
-            Console.WriteLine(string.Format("DONE in {0} ms", watcher.ElapsedMilliseconds));
-            
+            mux.Close(false);
+
+
 #if DEBUG
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
 #endif
 
+        }
+
+        static void Profile(string description, int iterations, Action act)
+        {
+            // clean up
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            // warm up
+            act();
+
+             var watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < iterations; i++)
+            {
+                act();
+            }
+            watch.Stop();
+
+            Console.WriteLine($"{description} => {watch.ElapsedMilliseconds:0.00} ms ({watch.ElapsedTicks:N0} ticks)");
         }
     }
 }
